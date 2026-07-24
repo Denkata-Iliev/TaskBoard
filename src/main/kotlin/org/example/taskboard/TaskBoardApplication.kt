2e8bf0aba61ff8f4ef4248a9bd8a4322c471ed03
Denislav Iliev <denislav.iliev@eveliko.com>
2026-07-24 13:12:10 +0300

    Initial commit

---
+++ src/main/kotlin/org/example/taskboard/TaskBoardApplication.kt
@@ -0,0 +1,11 @@
+package org.example.taskboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskBoardApplication>(*args)
+}
